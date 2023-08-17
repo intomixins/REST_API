d431965eccc54b7f086c632dd0b06fb0463042e0
maksgordt <intomixins@gmail.com>
2023-08-17 13:32:17 +0400

    change db

--- rest/manage.py
+++ rest/manage.py
@@ -3,13 +3,11 @@
 import os
 import sys
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project_name.settings.local")
-
 
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rest.settings')
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project_name.settings.local")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rest.settings.local")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
