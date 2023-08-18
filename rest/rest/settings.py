5ecf4eec3ebb260ab7b84112b2e4495017ca5d19
maksgordt <intomixins@gmail.com>
2023-08-18 12:54:44 +0400

    hope

--- rest/rest/settings.py
+++ rest/rest/settings.py
@@ -188,11 +188,12 @@ DJOSER = {
 }
 
 
-REDIS_HOST = 'red-cjebsvunk9qs7387m0v0'
-REDIS_PORT = '6379'
-CELERY_BROKER_URL = 'redis://' + REDIS_HOST + ':' + REDIS_PORT + '/0'
+# REDIS_HOST = 'red-cjebsvunk9qs7387m0v0'
+# REDIS_HOST = '0.0.0.0'
+# REDIS_PORT = '6379'
+# CELERY_BROKER_URL = 'redis://' + REDIS_HOST + ':' + REDIS_PORT + '/0'
 CELERY_BROKER_TRANSPORT_OPTIONS = {'visibility_timeout': 3600}
-CELERY_RESULT_BACKEND = 'redis://' + REDIS_HOST + ':' + REDIS_PORT + '/0'
+# CELERY_RESULT_BACKEND = 'redis://' + REDIS_HOST + ':' + REDIS_PORT + '/0'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
